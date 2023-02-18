5c77aa6c3f1477ffc4132eed7150fd85a05bbce1
4ce <88108711+4cecoder@users.noreply.github.com>
2023-02-18 12:09:28 -0500

    Create main.go

---
+++ src/main.go
@@ -0,0 +1,19 @@
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+
+mod api;
+mod ui;
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut term = Terminal::new(backend)?;
+
+    let query = ui::input()?;
+    let manga_list = api::search_manga(&query.trim())?;
+
+    ui::output(&mut term, &manga_list)?;
+
+    Ok(())
+}
